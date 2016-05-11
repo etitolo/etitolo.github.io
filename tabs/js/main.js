70263a725a34801ee9fd861804c688e4952ce9df
etitolo <etitolo@wsgc.com>
2016-05-11 13:06:25 -0700

    code review changes

--- tabs/js/main.js
+++ tabs/js/main.js
@@ -2,14 +2,10 @@
 function tabs() {
     var tabNumber = document.querySelectorAll(".tabs a");
     var tabContent = document.querySelectorAll(".tabContent div");
-
+    
     for (i=0; i < tabNumber.length; i++) {
         var el = tabNumber[i];
-        tabNumber[i].setAttribute("data-tab", "tab-" + i);
         el.addEventListener("click", toggleCurrentTab, false);
-    }
-    for (i=0; i < tabContent.length; i++) {
-        tabContent[i].classList.add("tab-" + i);
     }
     
     function toggleCurrentTab(e) {
@@ -22,7 +18,7 @@ function tabs() {
             removeCurrentContent.classList.remove("current");
         }
 
-        var tabId = $(this).attr('data-tab');
+        var tabId = $(this).attr('role');
         $("."+tabId).addClass('current');
         
         e.currentTarget.classList.add("current");
